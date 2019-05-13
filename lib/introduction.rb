6443af1e3d2039429f6aab996273301975f3d661
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-13 19:26:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end 
+name ("Chris")
+
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
